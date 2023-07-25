d602e34084c7f072bd8c6ccd6cd0f94bd8385f38
KyryloMuzychka <kyryll2016@gmail.com>
2023-07-25 16:48:01 +0300

    add: create AddStyle and className

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ClassCounter from './components/ClassCounter';
+import "./styles/App.css"
+import PostItem from './components/PostItem';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
+      <ClassCounter />
+      <PostItem post = {{id: 1, title: 'Javascript', body: 'Description'}}/>
+      <PostItem post = {{id: 2, title: 'Java', body: 'Des'}}/>      
     </div>
   );
 }
