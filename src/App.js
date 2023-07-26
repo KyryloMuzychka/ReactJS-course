719962d33be52a3b7eee3eec8e2db33b10529edb
KyryloMuzychka <kyryll2016@gmail.com>
2023-07-26 12:34:39 +0300

    add: component PostList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ClassCounter from './components/ClassCounter';
 import "./styles/App.css"
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+  ])
   return (
     <div className="App">
-      <ClassCounter />
-      <PostItem post = {{id: 1, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post = {{id: 2, title: 'Java', body: 'Des'}}/>      
+      <ClassCounter/>
+      <PostList posts={posts} title="List of items 1"/>
+      <PostList posts={posts} title="List of items 2"/>
     </div>
   );
+
 }
 
 export default App;
