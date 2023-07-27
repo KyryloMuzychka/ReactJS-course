e6c712a97d12bdbc75de078787fa147f731fc3a3
KyryloMuzychka <kyryll2016@gmail.com>
2023-07-27 14:52:43 +0300

    add: custom hooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButtton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,21 +14,10 @@ function App() {
     { id: 2, title: 'c', body: 'c' },
     { id: 3, title: 'b', body: 'a' },
   ])
-
-  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-  const sortedPosts = useMemo(() => {
-    console.log('getSortedPosts worked')
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -40,16 +30,16 @@ function App() {
 
   return (
     <div className="App">
-      <ClassCounter/>
-      <MyButtton style={{marginTop: 30}} onClick={()=>setModal(true)}>
+      <ClassCounter />
+      <MyButtton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Create post
       </MyButtton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
-      </MyModal>      
-      <hr style={{ margin: '15px 0' }} /> 
-      <PostFilter filter={filter} setFilter={setFilter}/>                   
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of items 1" />                                   
+      </MyModal>
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of items 1" />
     </div>
   );
 
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,20 @@
+import {useMemo} from 'react'
+
+export const useSortedPosts = (posts, sort) => {
+    const sortedPosts = useMemo(() => {
+        console.log('getSortedPosts worked')
+        if (sort) {
+          return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+      }, [sort, posts])
+    return sortedPosts  
+}
+
+export const usePosts = (posts, sort, query) => {
+    const sortedPosts = useSortedPosts(posts, sort)
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(query))
+      }, [query, sortedPosts])
+    return sortedAndSearchedPosts
+}
