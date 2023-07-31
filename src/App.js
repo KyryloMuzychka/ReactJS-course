ec321aa10a032c628ea44883f2e338fb30da63d4
KyryloMuzychka <kyryll2016@gmail.com>
2023-07-31 19:56:11 +0300

    add: BrowserRouter one page

--- src/App.js
+++ src/App.js
@@ -1,82 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import "./styles/App.css"
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import PostFilter from './components/PostFilter';
-import MyModal from './components/UI/MyModal/MyModal';
-import MyButtton from './components/UI/button/MyButton';
-import { usePosts } from './hooks/usePosts';
-import PostService from './components/API/PostService';
-import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { getPagesCount } from './utils/pages';
-import Pagination from './components/UI/pagination/Pagination';
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import "./styles/App.css";
+import AppRouter from './components/UI/AppRouter';
+import Navbar from './components/UI/Navbar/Navbar';
 
 function App() {
-
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false)
-  const [totalPages, setTotalPages] = useState(0)
-  const [limit, setLimit] = useState(10)
-  const [page, setPage] = useState(1)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll(limit, page)
-    setPosts(response.data)
-    const totalCount = response.headers['x-total-count']
-    setTotalPages(getPagesCount(totalCount, limit))
-  })
-  
-  
-  useEffect(() => {    
-    fetchPosts()
-  }, [page])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-  const changePage = (page) => {
-    setPage(page)        
-  }
-
   return (
-    <div className="App">
-      <ClassCounter />
-      <button onClick={fetchPosts}>Get posts</button>
-      <MyButtton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Create post
-      </MyButtton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-      />
-      {postError &&
-        <h1>Error: {postError}</h1>
-      }
-      {isPostLoading
-        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of items 1" />
-      }
-      <Pagination 
-        page={page}
-        changePage={changePage}
-        totalPages={totalPages}
-      />
-    </div>
-  );
-
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+        <div className="navbar__links">
+          <Link to="/about">About</Link>
+          <Link to="/posts">Posts</Link>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
